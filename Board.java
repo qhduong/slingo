d3ed09746c52970f31716f3da6a6d33ff1214dc4
qhduong <qhduong@uw.edu>
2012-11-30 21:19:48 -0800

    Constructor, fields, and constant for Board class.

--- Board.java
+++ Board.java
@@ -1,7 +1,15 @@
 public class Board {
 
-	public Board() {
+	private Tile[] board; // Stores board info
+	private boolean gameWon; // If there's a winning path
+	private static final int SIZE = 5; // Size x Size board
 
+	/**
+	* Creates a SIZE * SIZE game board.
+	*/
+	public Board() {
+		board = new Tile[SIZE * SIZE];
+		gameWon = false;
 	}
 
 }
