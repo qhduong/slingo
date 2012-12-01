a4db26a82eaeed6986209de74754711659d0d24c
qhduong <qhduong@uw.edu>
2012-12-01 00:45:31 -0800

    Simulate games until winning pattern is found.

--- SlingoMain.java
+++ SlingoMain.java
@@ -3,6 +3,15 @@ public class SlingoMain {
 	public static void main(String[] args) {
 		Board b = new Board();
 		b.print();
+
+		int i = 0;
+		while(!b.gameWon()) {
+			System.out.println("Round number: " + (i + 1));
+			b.play();
+			b.print();
+			System.out.println(b.gameWon());
+			i++;
+		}
 	}
 
 }
