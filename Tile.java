cbdf80139f4e9ab71bd466fb84435b68e012d231
qhduong <qhduong@uw.edu>
2012-11-30 21:18:07 -0800

    Constructor, fields, and constants for Tile class.

--- Tile.java
+++ Tile.java
@@ -1,7 +1,22 @@
 public class Tile {
 
-	public Tile(int value) {
+	private int value;
+	private boolean found;
+	private static final int MIN_VALUE = 1;
+	private static final int MAX_VALUE = 100;
 
+	/**
+  * Constructs a tile with given value.
+  *
+  * @param     value the value of this tile.
+  * @exception IllegalArgumentException if value is less than MIN_VALUE or greater than MAX_VALUE.
+  */
+	public Tile(int value) {
+		if(value < MIN_VALUE || value > MAX_VALUE) {
+			throw new IllegalArgumentException();
+		}
+		this.value = value;
+		found = false;
 	}
 
 }
